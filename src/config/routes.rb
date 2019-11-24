081aa182727de070552236d4ef62b354ce5722dc
System Administrator <sysadmin@hpc277.tcnj.edu>
2019-11-24 17:55:06 -0500

    Basic Functionality

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'contact/index'
-  get 'sell/index'
-  get 'welcome/index'
-  get 'browse/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :textbooks
+  resources :sessions, only: [:new, :create, :destroy]
 
+  get 'login', to: "sessions#new", as: "login"
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'contact', to: "contact#index", as: :contact_path
+  get 'sell', to: "textbooks#new", as: :sell_path
+  get 'welcome', to: "welcome#index", as: :welcome_path
+  get 'browse', to: "textbooks#browse", as: :browse_path
+  get 'browese_all', to: "textbooks#index", as: :browse_all_path
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
