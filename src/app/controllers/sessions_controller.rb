34340638137919b0d285a43331d1f9357ab4dc53
System Administrator <sysadmin@hpc277.tcnj.edu>
2019-11-25 11:43:52 -0500

    Peer Review Feedback

--- src/app/controllers/sessions_controller.rb
+++ src/app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_name(params[:name])
-    session[:user_id] = user.id
-    redirect_to root_url, notice: "Logged In"
-  end
+    if (user)
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged In"
+    else 
+      redirect_to login_path, notice: "Invalid User"
+    end 
+ end
+
 
   def destroy
     session[:user_id] = nil
